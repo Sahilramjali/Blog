552f291b31aa4a779963dd621af73758ba509ae4
Sahilramjali <sahil.ramjali13@gmail.com>
2023-06-30 14:10:47 +0545

    register page

--- client/src/components/authComponents/Login.jsx
+++ client/src/components/authComponents/Login.jsx
@@ -1,53 +1,56 @@
-
-import VisibilityIcon from '@mui/icons-material/Visibility';
-import { useState } from 'react';
-import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
-const InitialData={
-  username:'',
-  email:"",
-  password:'',
-}
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import { useState } from "react";
+import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
+const InitialData = {
+  email: "",
+  password: "",
+};
 const Login = () => {
-  
-  const[showPassword,setShowPassword]=useState(false);
-  const[Data,setData]=useState(InitialData)
-  const handleShowPassword=()=>{
+  const [showPassword, setShowPassword] = useState(false);
+  const [Data, setData] = useState(InitialData);
+  const handleShowPassword = () => {
     setShowPassword(!showPassword);
-  }
-  const handleInput=(e)=>{
-      
-      setData(prev=>({
-        ...prev,
-        [e.target.name]:e.target.value,
-      }));
-  
-    
-  }
-  const onSubmitHandler=(e)=>{
+  };
+  const handleInput = (e) => {
+    setData((prev) => ({
+      ...prev,
+      [e.target.name]: e.target.value,
+    }));
+  };
+  const onSubmitHandler = (e) => {
     e.preventDefault();
-      console.log(Data) ;
-      setData(InitialData)
-  }
+    console.log(Data);
+    setData(InitialData);
+  };
   return (
     <section className="login-div">
       <div className="Form-div">
-      <h2>Login</h2>
-      <form onSubmit={onSubmitHandler}>
-        <input onChange={handleInput} type="text" placeholder="Username" value={Data.username} name='username'/>
-        <input onChange={handleInput} type="email" placeholder="email" value={Data.email} name='email'/>
-        <div className='password'>
-        <input onChange={handleInput} type={showPassword?"text":"password"} placeholder="password" value={Data.password} name='password'/>
-        <div onClick={handleShowPassword}>
-        {
-          showPassword?<VisibilityOffIcon/>:<VisibilityIcon/>
-        }
-        </div>
-        </div>
-        <button type='submit'>Login</button>
-      </form>
+        <h2>Login</h2>
+        <form onSubmit={onSubmitHandler}>
+          <input
+            onChange={handleInput}
+            type="email"
+            placeholder="email"
+            value={Data.email}
+            name="email"
+          />
+          <div className="password">
+            <input
+              onChange={handleInput}
+              type={showPassword ? "text" : "password"}
+              placeholder="password"
+              value={Data.password}
+              name="password"
+            />
+            <div onClick={handleShowPassword}>
+              {showPassword ? <VisibilityOffIcon /> : <VisibilityIcon />}
+            </div>
+          </div>
+          <button type="submit">Login</button>
+        </form>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
--- client/src/components/authComponents/Register.jsx
+++ client/src/components/authComponents/Register.jsx
@@ -1,13 +1,64 @@
 
+import { VisibilityIcon,VisibilityOffIcon } from '@mui/icons-material/Visibility';
+import { useState } from 'react';
+const InitialData = {
+  username:"",
+  email: "",
+  password: "",
+};
 const Register = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  const [Data, setData] = useState(InitialData);
+  const handleShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+  const handleInput = (e) => {
+    setData((prev) => ({
+      ...prev,
+      [e.target.name]: e.target.value,
+    }));
+  };
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+    console.log(Data);
+    setData(InitialData);
+  };
   return (
-    <div>
-    
-      <form>
-        <input />
-      </form>
-    </div>
-  )
+    <section className="login-div">
+      <div className="Form-div">
+        <h2>Register</h2>
+        <form onSubmit={onSubmitHandler}>
+        <input
+            onChange={handleInput}
+            type="text"
+            placeholder="username"
+            value={Data.username}
+            name="username"
+          />
+          <input
+            onChange={handleInput}
+            type="email"
+            placeholder="email"
+            value={Data.email}
+            name="email"
+          />
+          <div className="password">
+            <input
+              onChange={handleInput}
+              type={showPassword ? "text" : "password"}
+              placeholder="password"
+              value={Data.password}
+              name="password"
+            />
+            <div onClick={handleShowPassword}>
+              {showPassword ? <VisibilityOffIcon /> : <VisibilityIcon />}
+            </div>
+          </div>
+          <button type="submit">Register</button>
+        </form>
+      </div>
+    </section>
+  );
 }
 
 export default Register;
