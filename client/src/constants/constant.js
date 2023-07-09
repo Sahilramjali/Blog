4ebf096da6f305e9570e8aa5ac109bcd46689b63
Sahilramjali <sahil.ramjali13@gmail.com>
2023-07-09 15:47:41 +0545

    url

--- client/src/constants/constant.js
+++ client/src/constants/constant.js
@@ -1,8 +1,8 @@
 
 // export const GLOBAL_URL='http://localhost:4000/api/';
 export const GLOBAL_URL='https://blog-api-n765.onrender.com/api/'
-export const REGISTER_API_URL=GLOBAL_URL+"signup";
-export const LOGIN_API_URL=GLOBAL_URL+"login";
+export const REGISTER_API_URL=GLOBAL_URL+"auth/signup";
+export const LOGIN_API_URL=GLOBAL_URL+"auth/login";
 export const POST_API_URL=GLOBAL_URL+"blog/post";
 export const GET_BLOG_API_URL=GLOBAL_URL+"blog/getblog";
 export const GET_SINGLE_BLOG_API_URL=GLOBAL_URL+"blog/getblog/"
