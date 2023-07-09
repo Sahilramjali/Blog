d0815946ba8d22d0570174880a4b2335b2dc6889
Sahilramjali <sahil.ramjali13@gmail.com>
2023-07-09 15:21:15 +0545

    url

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,12 @@
 const express=require('express');
 
 const router=express.Router();
-
+router.use(
+    cors({
+        credentials:true,
+        origin:process.env.FRONTENT_WEBSITE,
+    })
+);
 router.use('/auth',require('./auth.route.js'));
 router.use('/blog',require("./blog.route.js"))
 
