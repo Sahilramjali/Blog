e573319f5093f926c3a611bead845bf58bfbb217
Sahilramjali <sahil.ramjali13@gmail.com>
2023-07-12 10:10:42 +0545

    seo

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,22 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { HashRouter } from 'react-router-dom'
-import { UserProvider } from './hooks/userInfo.jsx'
-import  {HelmetProvider}  from 'react-helmet-async'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { HashRouter } from "react-router-dom";
+import { UserProvider } from "./hooks/userInfo.jsx";
+import { HelmetProvider } from "react-helmet-async";
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HelmetProvider>
-      <HashRouter>
+    <HashRouter>
+      <HelmetProvider>
         <UserProvider>
           <App />
         </UserProvider>
-
-      </HashRouter>
-    </HelmetProvider>
-
-  </React.StrictMode>,
-)
+      </HelmetProvider>
+    </HashRouter>
+  </React.StrictMode>
+);
